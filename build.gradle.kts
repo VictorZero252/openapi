345f0920681d71754cb47db285312e7d5da0c326
victor <you@example.com>
2024-12-13 13:35:39 +0300

    issue-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.openapi.generator") version "7.0.1"
 }
 
 group = "org.vg"
@@ -28,6 +29,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.moshi:moshi:1.15.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -45,4 +49,21 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+openApiGenerate {
+    inputSpec.set("$rootDir/src/main/resources/service-api.yml")
+    generatorName.set("kotlin")
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("${buildDir}/generate-resources/main/src")
+        }
+    }
+}
+
+tasks.compileKotlin{
+    dependsOn("openApiGenerate")
 }
